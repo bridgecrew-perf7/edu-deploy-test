6bf5896df007810afa6424544283b0d89077cc51
toantqt <toantqt0609@gmail.com>
2021-05-06 09:26:18 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Route, Redirect, BrowserRouter } from "react-router-dom";
+import { Route, Redirect, HashRouter } from "react-router-dom";
 import PageHomeView from "./Page/Home/PageHomeView";
 import PageCourseView from "./Page/Course/Page-Course-View.component";
 import PageArchivesView from "./Page/Archives/Page-Archives-View.component";
 function App() {
   return (
-    <BrowserRouter>
-      <Route path="/home" component={PageHomeView}></Route>
+    <HashRouter>
+      <Route exact path="/" component={PageHomeView}></Route>
       <Route path="/course" component={PageCourseView}></Route>
       <Route path="/archives" component={PageArchivesView}></Route>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
